82d33f629248108e29272d7d2cab070b97837a09
toki97 <utokajuk@wp.pl>
2021-06-16 00:08:34 +0200

    Updated styles

--- src/components/SongsList/styles.ts
+++ src/components/SongsList/styles.ts
@@ -1,7 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  list: { display: "grid", rowGap: "2rem" },
+  list: { display: "grid", rowGap: "1rem" },
   albumView: {
     rowGap: "1rem",
   },
